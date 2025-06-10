79fa12a7cf8e571f1549e7ff33793d56a2f7e5e2
HIEU DOAN <49260251+hieudoanm@users.noreply.github.com>
2025-06-10 17:42:55 +0700

    update document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,13 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html className="dark" lang="en">
-      <Head />
+      <Head>
+        <meta name="theme-color" content="#171717" />
+        {/* <!-- iOS --> */}
+        <meta name="apple-mobile-web-app-title" content="Atomic" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-touch-fullscreen" content="yes" />
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
